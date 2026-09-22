ef57a389cb629f58051969934868b88e65f3383a
Ninad Deodhare <2025.ninadd@isu.ac.in>
2026-09-22 12:58:53 +0530

    Add root GET route

--- Ninad 150096725180/server.js
+++ Ninad 150096725180/server.js
@@ -21,6 +21,10 @@ app.use('/api/auth', authRoutes);
 app.use('/api/events', eventRoutes);
 app.use('/api/tickets', ticketRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Event Management API is running!');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
